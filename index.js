03467cb5d40a70beafabf630f793ed2f42d856d4
Justice Maravilla <justicerafael@gmail.com>
2017-04-12 17:05:11 -0500

    HTML Progress
    Here's the html I have so far

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = express()
+var path = require('path')
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '/Information-Page.html'))
+});
+
 
 app.listen(8080, function(){
-  console.log('Scouting Web :)')
+  console.log('Scouting Web Launched!')
 })
